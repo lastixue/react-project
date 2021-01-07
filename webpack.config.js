51113227bd3b1eabb2a0c6acaa6f1ec57efe6c08
xue87 <s18313109@stu.edu.tw>
2021-01-07 19:33:42 +0800

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+ 
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader:'babel-loader'
+        },
+      ],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path:path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './dist/index.html'
+   })
+ 
+  ],
+  performance: { hints: false },
+  devServer: {
+    contentBase: './dist',
+    port: 8000,
+    open:true,
+    hot:true
+  },
+};
